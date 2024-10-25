c88f0b924b083846112ee10f72c2212e5ea82251
Michael Redig <mredig@gmail.com>
2024-10-25 16:25:32 -0500

    (fix) tests without generic typing

--- Tests/RationalModuleTests/IntegerLiteralTests.swift
+++ Tests/RationalModuleTests/IntegerLiteralTests.swift
@@ -4,7 +4,7 @@ import XCTest
 
 final class IntegerLiteralTests: XCTestCase {
 	func test_literal_2_returns_2_over_1() throws {
-		let r: Rational<Int> = 2
+		let r: Rational = 2
 		XCTAssertEqual(r.numerator, 2)
 		XCTAssertEqual(r.denominator, 1)
 	}
--- Tests/RationalModuleTests/LosslessStringConvertibleTests.swift
+++ Tests/RationalModuleTests/LosslessStringConvertibleTests.swift
@@ -4,36 +4,38 @@ import XCTest
 
 final class LosslessStringConvertibleTests: XCTestCase {
 	func test_2_returns_2_over_1() throws {
-		let r = Rational<Int>("2")
+		let r = Rational("2")
 		XCTAssertNotNil(r)
 		XCTAssertEqual(r?.numerator, 2)
 		XCTAssertEqual(r?.denominator, 1)
 	}
 
+	func test_2_over_4_returns_2_over_4_when_not_reduced_by_default() throws {
+		let r = Rational("2/4")
+		XCTAssertNotNil(r)
+		XCTAssertEqual(r?.numerator, 2)
+		XCTAssertEqual(r?.denominator, 4)
+	}
+
 	func test_2_over_4_returns_1_over_2() throws {
-		let r = Rational<Int>("2/4")
+		let r = Rational("2/4", reduced: true, normalizedSign: false)
 		XCTAssertNotNil(r)
 		XCTAssertEqual(r?.numerator, 1)
 		XCTAssertEqual(r?.denominator, 2)
 	}
 
 	func test_whitespace_returns_nil() throws {
-		let r = Rational<Int>("2 / 3")
+		let r = Rational("2 / 3")
 		XCTAssertNil(r)
 	}
 
 	func test_missing_denominator_returns_nil() throws {
-		let r = Rational<Int>("2/")
+		let r = Rational("2/")
 		XCTAssertNil(r)
 	}
 
 	func test_division_by_0_returns_nil() throws {
-		let r = Rational<Int>("1/0")
-		XCTAssertNil(r)
-	}
-
-	func test_128_over_2_returns_nil_for_int8() throws {
-		let r = Rational<Int8>("128/2")
+		let r = Rational("1/0")
 		XCTAssertNil(r)
 	}
 }
