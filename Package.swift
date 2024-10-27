3cc944b9f575ff338ccb2f2e4dae9126cdf2699e
Michael Redig <mredig@gmail.com>
2024-10-27 04:22:40 -0500

    (refactor) remove extra dependency

--- Package.swift
+++ Package.swift
@@ -13,10 +13,6 @@ let package = Package(
 	],
 	dependencies: [
 		.package(
-			url: "https://github.com/apple/swift-format.git",
-			exact: "509.0.0"
-		),
-		.package(
 			url: "https://github.com/apple/swift-numerics",
 			from: "1.0.0"
 		),
