385f20102e0bd3b31e3a73f71ec9f1c569a6ddd8
Michael Redig <mredig@gmail.com>
2024-11-10 17:30:40 -0600

    (refactor) organize elementary tests

--- Tests/BigRationalModuleTests/ElementaryTests.swift
+++ Tests/BigRationalModuleTests/ElementaryTests.swift
@@ -4,6 +4,71 @@ import BigInt
 import Testing
 
 struct ElementaryTests {
+	@Test func atan2() {}
+	@Test func erf() {}
+	@Test func erfc() {}
+	@Test func exp2() {}
+	@Test func exp10() {}
+	@Test func hypot() {}
+	@Test func gamma() {}
+	@Test func log2() {}
+	@Test func log10() {}
+	@Test func logGamma() {}
+	@Test func signGamma() {}
+	@Test func _mulAdd() {}
+	@Test func exp() {}
+	@Test func expMinusOne() {}
+	@Test func cosh() {}
+	@Test func sinh() {}
+	@Test func tanh() {}
+	@Test func cos() {}
+	@Test func sin() {}
+	@Test func tan() {}
+	@Test func log() {}
+	@Test func logOnePlus() {}
+	@Test func acosh() {}
+	@Test func asinh() {}
+	@Test func atanh() {}
+	@Test func acos() {}
+	@Test func asin() {}
+	@Test func atan() {}
+
+	@Test func nthPower() throws {
+		#expect(Rational.pow(2, 2 as Int) == 4)
+		#expect(Rational.pow(-2, 2 as Int) == 4)
+		#expect(Rational.pow(2, 3 as Int) == 8)
+		#expect(Rational.pow(-2, 3 as Int) == -8)
+		#expect(Rational.pow(-3, -1 as Int) == Rational(-1, 3))
+		#expect(Rational.pow(3, 8 as Int) == 6561)
+		#expect(Rational.pow(3, 9 as Int) == 19683)
+		#expect(Rational.pow(2, -2 as Int) == Rational(1, 4))
+		#expect(Rational.pow(2, -3 as Int) == Rational(1, 8))
+		#expect(Rational.pow(-2, -3 as Int) == Rational(-1, 8))
+		#expect(Rational.pow(Rational(1, 1_000_000), -1 as Int) == 1_000_000)
+		#expect(Rational.pow(Rational(1, 2), -1 as Int) == 2)
+		#expect(Rational.pow(Rational(1, 2), 3 as Int) == Rational(1, 8))
+		#expect(Rational.pow(Rational.zero, 3 as Int) == .zero)
+		#expect(Rational.pow(Rational.zero, -1 as Int).isNaN)
+		#expect(Rational.pow(Rational.zero, -3 as Int).isNaN)
+		#expect(Rational.pow(Rational.zero, 0 as Int) == 1)
+		#expect(Rational.pow(Rational.nan, 0 as Int).isNaN)
+		#expect(Rational.pow(Rational.nan, 1 as Int).isNaN)
+		#expect(Rational.pow(Rational.nan, 5 as Int).isNaN)
+		#expect(Rational.pow(10, 9 as Int) == 1_000_000_000)
+		#expect(Rational.pow(10, 0 as Int) == 1)
+		#expect(Rational.pow(10, 1 as Int) == 10)
+		#expect(Rational.pow(10, -1 as Int) == Rational(1, 10))
+		#expect(Rational.pow(10, -9 as Int) == Rational(1, 1_000_000_000))
+	}
+
+	@Test func nthFractionPower() throws {
+		#expect(Rational.pow(-8, Rational(1, 3)) == -2)
+		#expect(Rational.pow(8, Rational(2, 3)) == 4)
+		#expect(Rational.pow(9, Rational(1, 2)) == 3)
+		#expect(Rational.pow(9, Rational(1, 1)) == 9)
+		#expect(Rational.pow(9, Rational.zero) == 1)
+	}
+
 	@Test func squareRoot() throws {
 		#expect(Rational.sqrt(Rational(1)) == 1)
 		#expect(Rational.sqrt(Rational(4)) == 2)
@@ -76,41 +141,5 @@ struct ElementaryTests {
 		#expect(Rational.root(Rational(8, 27), 3) == Rational(2, 3))
 		#expect(Rational.root(Rational(8, 27), -3) == Rational(3, 2))
 		#expect(Rational.root(Rational(-8, 27), -3) == Rational(-3, 2))
-	}
-
-	@Test func nthPower() throws {
-		#expect(Rational.pow(2, 2 as Int) == 4)
-		#expect(Rational.pow(-2, 2 as Int) == 4)
-		#expect(Rational.pow(2, 3 as Int) == 8)
-		#expect(Rational.pow(-2, 3 as Int) == -8)
-		#expect(Rational.pow(-3, -1 as Int) == Rational(-1, 3))
-		#expect(Rational.pow(3, 8 as Int) == 6561)
-		#expect(Rational.pow(3, 9 as Int) == 19683)
-		#expect(Rational.pow(2, -2 as Int) == Rational(1, 4))
-		#expect(Rational.pow(2, -3 as Int) == Rational(1, 8))
-		#expect(Rational.pow(-2, -3 as Int) == Rational(-1, 8))
-		#expect(Rational.pow(Rational(1, 1_000_000), -1 as Int) == 1_000_000)
-		#expect(Rational.pow(Rational(1, 2), -1 as Int) == 2)
-		#expect(Rational.pow(Rational(1, 2), 3 as Int) == Rational(1, 8))
-		#expect(Rational.pow(Rational.zero, 3 as Int) == .zero)
-		#expect(Rational.pow(Rational.zero, -1 as Int).isNaN)
-		#expect(Rational.pow(Rational.zero, -3 as Int).isNaN)
-		#expect(Rational.pow(Rational.zero, 0 as Int) == 1)
-		#expect(Rational.pow(Rational.nan, 0 as Int).isNaN)
-		#expect(Rational.pow(Rational.nan, 1 as Int).isNaN)
-		#expect(Rational.pow(Rational.nan, 5 as Int).isNaN)
-		#expect(Rational.pow(10, 9 as Int) == 1_000_000_000)
-		#expect(Rational.pow(10, 0 as Int) == 1)
-		#expect(Rational.pow(10, 1 as Int) == 10)
-		#expect(Rational.pow(10, -1 as Int) == Rational(1, 10))
-		#expect(Rational.pow(10, -9 as Int) == Rational(1, 1_000_000_000))
-	}
-
-	@Test func nthFractionPower() throws {
-		#expect(Rational.pow(-8, Rational(1, 3)) == -2)
-		#expect(Rational.pow(8, Rational(2, 3)) == 4)
-		#expect(Rational.pow(9, Rational(1, 2)) == 3)
-		#expect(Rational.pow(9, Rational(1, 1)) == 9)
-		#expect(Rational.pow(9, Rational.zero) == 1)
 	}
 }
